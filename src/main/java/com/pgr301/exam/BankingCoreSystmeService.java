3c2d9b76239a85165747785c397598cee94f4772
TEKami13 <tobias.krogh@hotmail.com>
2024-09-06 09:39:27 +0000

    added

--- src/main/java/com/pgr301/exam/BankingCoreSystmeService.java
+++ src/main/java/com/pgr301/exam/BankingCoreSystmeService.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.math.BigDecimal;
 
 public interface BankingCoreSystmeService {
-
+    
     void transfer(Transaction tx, String fromAccount, String toAccount);
 
     Account updateAccount(Account a);

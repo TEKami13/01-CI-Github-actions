14883f3d09aa94c5286b188dd3619019774a99a3
TEKami13 <tobias.krogh@hotmail.com>
2024-09-06 10:01:53 +0000

    compilation error introduced

--- src/main/java/com/pgr301/exam/BankAccountApplication.java
+++ src/main/java/com/pgr301/exam/BankAccountApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BankAccountApplication {
     public static void main(String[] args) {
         SpringApplication.run(BankAccountApplication.class, args);
-        private String pop ="";
+        private string pop ="";
         
     }
 }

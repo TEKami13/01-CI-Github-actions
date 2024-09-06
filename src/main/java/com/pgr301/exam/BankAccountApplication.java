5c94aac5aa1ad37783de7c2e8149919860d54cd9
TEKami13 <tobias.krogh@hotmail.com>
2024-09-06 09:27:09 +0000

    added text

--- src/main/java/com/pgr301/exam/BankAccountApplication.java
+++ src/main/java/com/pgr301/exam/BankAccountApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BankAccountApplication {
+    system.out.println("Hey sexy!");
     public static void main(String[] args) {
         SpringApplication.run(BankAccountApplication.class, args);
     }
